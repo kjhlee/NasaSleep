7a8c726fb97fe17e58f3733b8b9a7cb7476b91fb
theemotionalpie <52635340+theemotionalpie@users.noreply.github.com>
2020-10-04 19:20:02 -0700

    Wakeshift is an app that uses data from the wakeshift mask
    The Wakeshift Mask and Wakeshift App work conjunctively in order to reduce jet lag-related fatigue for space and Earth travelers alike, while also empowering users to maintain a balance of nutrition, fitness, and sleep. The eye mask contains an electroencephalogram (EEG) and reflective pulse oximetry sensors in the forehead area, as well as an electrooculography (EOG) sensor in the eye area. Together, the EEG and EOG detect the stage of sleep the user resides in and determines when and which binaural beats to play. The app consolidates nutrition, exercise, and sleep information to improve restfulness for sleep-deprived individuals.

---
+++ NasaSleep/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NasaSleep
+//
+//  Created by KJ Lee on 10/3/20.
+//  Copyright © 2020 KJ Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
